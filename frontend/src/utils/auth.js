ce53676c891835167868bd95506412912be33bf0
Haider3505 <haider30555@gmail.com>
2023-08-16 12:55:47 -0700

    adding route protection

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,8 +1,17 @@
+import { redirect } from "react-router-dom"
 
 const getAuthToken = () => {
     return localStorage.getItem('token')
 }
 
 export const tokenLoader = () => getAuthToken()
+
+export const checkAuthLoader = () => {
+    const token = getAuthToken()
+    if (!token)
+        return redirect('/')
+
+    return null
+}
 
 export default getAuthToken
